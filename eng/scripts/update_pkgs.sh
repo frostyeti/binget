73993d61d1981e39417b73cea5749a4571562bdb
grimfrostmage <180069990+grimfrostmage@users.noreply.github.com>
2026-02-26 08:39:37 -0600

    chore: scaffold package update automation script

---
+++ eng/scripts/update_pkgs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# TODO: Automate fetching the latest GitHub release tags for packages in binget-pkgs
+# TODO: Update versions.json for each package
+# TODO: Clone github.com/frostyeti/binget-pkgs using the `master` branch
+# TODO: Generate manifest.<os>-<arch>.json files using Goreleaser assets structure
+# TODO: Commit and push changes back to binget-pkgs master branch
+
+echo "This script is a placeholder for the binget-pkgs automation loop."
